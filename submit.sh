0ec340da292797abea396863ee5ac1caf84fc6f9
Chris Howard <christopherh@email.arizona.edu>
2017-12-13 18:37:18 -0700

    Add submit script

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash #
+
+qsub year.sh #
+qsub jan.sh #
+qsub feb.sh #
+qsub mar.sh #
+qsub apr.sh #
+qsub may.sh #
+qsub jun.sh #
+qsub jul.sh #
+qsub aug.sh #
+qsub sep.sh #
+qsub oct.sh #
+qsub nov.sh #
+qsub dec.sh #
